7dc869830755e8fdd79340ff9dd3bfa527c202fe
MarcoJanisch90 <johnny-jarco@gmx.de>
2019-06-17 22:33:27 +0200

    Styling Events

--- public/js/events.js
+++ public/js/events.js
@@ -1,23 +1,31 @@
 //function to get database entrys appear on events.html
 eventDB.allDocsOfLocalDB.then(function(result) {
     for (const entry of result.rows) {
+
+      //div for dates
+      const newDate = document.createElement("h4");
+      newDate.innerHTML = entry.doc.date;
+      newDate.className = "eventDates";
+      //document.getElementById('events').append(newDate);
+
       const newDiv = document.createElement("div");
-      const newF = document.createElement("img");
-      const newD = document.createElement("p");
-      const newH = document.createElement("h2");
-      const newS = document.createElement("h3");
-      //const newP = document.createElement("p");
-      newF.innerHTML = entry.doc.attachmentName;
-      newD.innerHTML = entry.doc.date;
+      //const newF = document.createElement("img");
+      const newH = document.createElement("h3");
+      const newS = document.createElement("p");
+      //newF.innerHTML = entry.doc.attachmentName;
       newH.innerHTML = entry.doc.title;
       newS.innerHTML = entry.doc.summary;
-      //newP.innerHTML = entry.doc.description;
-      newDiv.append(newF);
-      newDiv.append(newD);
+      //newDiv.append(newF);
       newDiv.append(newH);
       newDiv.append(newS);
-      //newDiv.append(newP);
-      document.getElementById('events').append(newDiv);
+      newDiv.className = "eventList";
+      //document.getElementById('events').append(newDiv);
+
+      const eventColumn = document.createElement("div");
+      eventColumn.append(newDate);
+      eventColumn.append(newDiv);
+      eventColumn.className = "eventColumn";
+      document.getElementById('events').append(eventColumn);
 /*       if (entry.doc._attachments !== undefined) {
         locationDB._getAttachment(entry.doc._id, Object.keys(entry.doc._attachments)[0]).then(function(blob){
           console.log("test");
